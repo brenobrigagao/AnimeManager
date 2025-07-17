051d02ca1eee15351f2c07d9fbe222382399c60a
Breno Brigagão <brenobrigagaodev@gmail.com>
2025-07-17 16:04:25 -0300

    add os AddScoped

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,16 @@
 using Api;
+using Application.Services.Anime;
 using Application.Services.Auth;
+using Application.Services.Avaliação;
+using Application.Services.Avaliacao;
+using Application.Services.Estudio;
+using Application.Services.Genero;
 using Application.Services.Senha;
+using Application.Services.Token;
+using Application.Services.Usuario;
 using Infra.Data.Context;
+using Infra.Repositories;
+using Infra.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
 
@@ -9,8 +18,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IAnimeService, AnimeService>();
+builder.Services.AddScoped<IAvaliacaoService, AvaliacaoService>();
+builder.Services.AddScoped<IEstudioService, EstudioService>();
+builder.Services.AddScoped<IGeneroService, GeneroService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ISenhaService,SenhaService>();
+builder.Services.AddScoped<ISenhaService, SenhaService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IAnimeRepository, AnimeRepository>();
+builder.Services.AddScoped<IAvaliacaoRepository, AvaliacaoRepository>();
+builder.Services.AddScoped<IEstudioRepository, EstudioRepository>();
+builder.Services.AddScoped<IGeneroRepository, GeneroRepository>();
+builder.Services.AddScoped<IUnityOfWork, UnityOfWork>();
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
